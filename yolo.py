0a6934a6edffb4152e084934bb759da3a872cc14
rytst <r.saito0111@gmail.com>
2024-06-29 19:01:15 +0900

    first commit

---
+++ yolo.py
@@ -0,0 +1,33 @@
+import cv2
+import urllib.request
+import os
+import time
+
+from ultralytics import YOLO
+
+
+
+os.environ["XDG_SESSION_TYPE"] = "xcb"
+save_as = 'image.jpg'
+url = 'http://192.168.128.114/capture'
+model = YOLO("yolov8n.pt")
+
+while True:
+    time.sleep(5)
+
+    urllib.request.urlretrieve(url, save_as)
+
+    image = cv2.imread(save_as)
+
+    results = model(image)
+
+    annotated_image = results[0].plot()
+
+    cv2.imshow("Image", annotated_image)
+
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+
